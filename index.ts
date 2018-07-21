b395e6bc76b4edec68a830494dd41e43bf26bfb1
Leo Young <420760135@qq.com>
2018-07-21 23:23:19 +0800

    refactor: format

--- index.ts
+++ index.ts
@@ -1,23 +1,28 @@
 import http, { IncomingMessage, RequestOptions } from 'http';
 import url from 'url';
 
-http.createServer().on('request', (req: IncomingMessage, res: http.ServerResponse) => {
-  // tslint:disable-next-line:no-console
-  console.log(req.url);
-  const urlObj = url.parse(req.url as string);
-  const options: RequestOptions = {
-    hostname: urlObj.hostname,
-    port: urlObj.port || 80,
-    path: urlObj.path,
-    method: req.method,
-    headers: req.headers
-  };
-  const proxyReq = http.request(options, (proxyRes: IncomingMessage) => {
-    res.writeHead(proxyRes.statusCode as number, proxyRes.headers);
-    proxyRes.pipe(res);
-  }).on('error', () => {
-    res.end();
-  });
+http
+  .createServer()
+  .on('request', (req: IncomingMessage, res: http.ServerResponse) => {
+    // tslint:disable-next-line:no-console
+    console.log(req.url);
+    const urlObj = url.parse(req.url as string);
+    const options: RequestOptions = {
+      hostname: urlObj.hostname,
+      port: urlObj.port || 80,
+      path: urlObj.path,
+      method: req.method,
+      headers: req.headers
+    };
+    const proxyReq = http
+      .request(options, (proxyRes: IncomingMessage) => {
+        res.writeHead(proxyRes.statusCode as number, proxyRes.headers);
+        proxyRes.pipe(res);
+      })
+      .on('error', () => {
+        res.end();
+      });
 
-  req.pipe(proxyReq);
-}).listen(7269, '0.0.0.0');
+    req.pipe(proxyReq);
+  })
+  .listen(7269, '0.0.0.0');
