0eadca42bb0677a95ff1d5deb31dec9eacb46a85
Leo Young <420760135@qq.com>
2018-07-22 18:30:40 +0800

    feat: add before send req/res

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
-import http, { IncomingMessage, RequestOptions } from 'http';
+import http, { IncomingMessage, RequestOptions, ServerResponse } from 'http';
 import url from 'url';
+
+const beforeSendRequest = async (req: IncomingMessage) => {
+  // tslint:disable-next-line:no-console
+  console.log('........req:' + JSON.stringify(req.headers));
+  return req;
+};
+
+const beforeSendResponse = async (res: IncomingMessage) => {
+  // tslint:disable-next-line:no-console
+  console.log('........res:' + JSON.stringify(res.headers));
+  return res;
+};
 
 http
   .createServer()
-  .on('request', (req: IncomingMessage, res: http.ServerResponse) => {
-    // tslint:disable-next-line:no-console
-    console.log(req.url);
+  .on('request', async (req: IncomingMessage, res: ServerResponse) => {
     const urlObj = url.parse(req.url as string);
     const options: RequestOptions = {
       hostname: urlObj.hostname,
@@ -15,14 +25,15 @@ http
       headers: req.headers
     };
     const proxyReq = http
-      .request(options, (proxyRes: IncomingMessage) => {
+      .request(options, async (proxyRes: IncomingMessage) => {
+        proxyRes = await beforeSendResponse(proxyRes);
         res.writeHead(proxyRes.statusCode as number, proxyRes.headers);
         proxyRes.pipe(res);
       })
       .on('error', () => {
         res.end();
       });
-
+    req = await beforeSendRequest(req);
     req.pipe(proxyReq);
   })
   .listen(7269, '0.0.0.0');
