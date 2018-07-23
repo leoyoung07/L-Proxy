bb31c1d6472cb2aa37ea21f116eecdaed4f8037b
Leo Young <420760135@qq.com>
2018-07-24 01:03:52 +0800

    feat: add request id

--- index.ts
+++ index.ts
@@ -1,4 +1,16 @@
+import { IncomingMessage } from 'http';
 import LProxy from './proxy';
 
-const proxy = new LProxy();
+const beforeSendRequest = async (requestId: string, req: IncomingMessage) => {
+  // tslint:disable-next-line:no-console
+  console.log('........req:' + requestId + '\n' + JSON.stringify(req.headers));
+  return req;
+};
+const beforeSendResponse = async (requestId: string, res: IncomingMessage) => {
+  // tslint:disable-next-line:no-console
+  console.log('........res:' + requestId + '\n' + JSON.stringify(res.headers));
+  return res;
+};
+
+const proxy = new LProxy(beforeSendRequest, beforeSendResponse);
 proxy.start();
--- proxy.ts
+++ proxy.ts
@@ -2,20 +2,20 @@ import http, { IncomingMessage, RequestOptions, ServerResponse } from 'http';
 import url from 'url';
 export default class LProxy {
 
+  private requestId = 0;
+
+  constructor(
+    private beforeSendRequest = async (id: string, req: IncomingMessage) => req,
+    private beforeSendResponse = async (id: string, res: IncomingMessage) => res
+  ) {
+
+  }
+
   public start() {
-    const beforeSendRequest = async (req: IncomingMessage) => {
-      // tslint:disable-next-line:no-console
-      console.log('........req:' + JSON.stringify(req.headers));
-      return req;
-    };
-    const beforeSendResponse = async (res: IncomingMessage) => {
-      // tslint:disable-next-line:no-console
-      console.log('........res:' + JSON.stringify(res.headers));
-      return res;
-    };
     http
     .createServer()
     .on('request', async (req: IncomingMessage, res: ServerResponse) => {
+      const requestId = this.getRequestId();
       const urlObj = url.parse(req.url as string);
       const options: RequestOptions = {
         hostname: urlObj.hostname,
@@ -26,16 +26,20 @@ export default class LProxy {
       };
       const proxyReq = http
         .request(options, async (proxyRes: IncomingMessage) => {
-          proxyRes = await beforeSendResponse(proxyRes);
+          proxyRes = await this.beforeSendResponse(requestId, proxyRes);
           res.writeHead(proxyRes.statusCode as number, proxyRes.headers);
           proxyRes.pipe(res);
         })
         .on('error', () => {
           res.end();
         });
-      req = await beforeSendRequest(req);
+      req = await this.beforeSendRequest(requestId, req);
       req.pipe(proxyReq);
     })
     .listen(7269, '0.0.0.0');
+  }
+
+  private getRequestId() {
+    return (this.requestId++).toString();
   }
 }
